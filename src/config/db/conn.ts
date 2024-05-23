175db5ba66a1c1f8b186b1300441d9bdf4c6c94c
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-05-22 19:40:13 -0500

    refactor archivo conn

---
+++ src/config/db/conn.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = new Sequelize(
+    process.env.DB_NAME || 'mysql',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || '', {
+    port: parseInt(process.env.DB_PORT || '3306') || 3306,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+});
+
+export default db;
