b1ca80476215329ce9bc036e4f1d0162301f10ea
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-10-30 20:06:11 -0500

    Definición de la tabla en la migración

--- migrations/20240928021458-first-migration.js
+++ migrations/20240928021458-first-migration.js
@@ -1,11 +1,12 @@
 const { EncryptionUtil } = require('../dist/lib-core/utils/encryption.util');
 
+const { createTableCharacters } = require('./tables/characters.js');
 const { createTableOAuthClients } = require('./tables/oauth-clients.js');
-const { createTableUsers } = require('./tables/users.js');
-const { createTableRoles } = require('./tables/roles.js');
 const { createTablePersons } = require('./tables/persons.js');
-const { createTableServices } = require('./tables/services.js');
+const { createTableRoles } = require('./tables/roles.js');
 const { createTableRolesServices } = require('./tables/roles-services.js');
+const { createTableServices } = require('./tables/services.js');
+const { createTableUsers } = require('./tables/users.js');
 
 const { bulkInsertOAuthClients } = require('./bulk/oauth-clients.js');
 const { bulkInsertUsers } = require('./bulk/users.js');
@@ -30,6 +31,7 @@ module.exports = {
     await createTablePersons(queryInterface, Sequelize);
     await createTableServices(queryInterface, Sequelize);
     await createTableRolesServices(queryInterface, Sequelize);
+    await createTableCharacters(queryInterface, Sequelize);
 
     // Data initial migration
 
