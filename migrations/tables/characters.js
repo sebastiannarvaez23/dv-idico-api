355987af6591c60db53a5ca73b4c75b550ff2bdd
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-10-30 20:05:34 -0500

    Creación de tabla characters en las migraciones

---
+++ migrations/tables/characters.js
@@ -0,0 +1,60 @@
+module.exports.createTableCharacters = async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('characters', {
+        id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+        },
+        name: {
+            type: Sequelize.STRING(100),
+            allowNull: false,
+            unique: true,
+        },
+        image: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            unique: true,
+        },
+        age: {
+            type: Sequelize.STRING(3),
+            allowNull: false,
+            unique: false,
+        },
+        history: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+            unique: false,
+        },
+        created_by: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        updated_by: {
+            type: Sequelize.UUID,
+            allowNull: true,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.fn('NOW'),
+        },
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.fn('NOW'),
+        },
+        deleted_at: {
+            type: Sequelize.DATE,
+            allowNull: true,
+        }
+    });
+}
