5041861d3016f71a0a9d75b255139b4a62ee88ae
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-10-31 19:44:30 -0500

    Entidades y control de errores Products

---
+++ src/lib-entities/products/product/product.entity.ts
@@ -0,0 +1,17 @@
+export type Qualification = '1' | '2' | '3' | '4' | '5';
+
+export interface ProductEntity {
+    id?: string;
+    title?: string;
+    image?: string;
+    createdDate?: Date;
+    qualification?: Qualification;
+    genderId?: string;
+    kindId?: string;
+    createdBy?: string;
+    updatedBy?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+
+}
