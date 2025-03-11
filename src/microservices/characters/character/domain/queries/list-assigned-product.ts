22934b42d193fdd586b9384b3ea21959e1b92a6e
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2025-03-10 20:19:16 -0500

    Lista de selección terminada

--- src/microservices/characters/character/domain/queries/list-assigned-product.ts
+++ src/microservices/characters/character/domain/queries/list-assigned-product.ts
@@ -1,10 +1,17 @@
 export const queryListAssignedProduct = `
     SELECT
-        T0.id, T0.name, T0.image, T0.age, T0.history, 
-        (CASE WHEN character_id IS NULL THEN false ELSE true END) AS assigned,
-        T0.created_by, T0.updated_by
+        T0.id,
+        T0.name,
+        T0.image,
+        T0.age,
+        T0.history,
+        T0.created_by,
+        T0.updated_by,
+        (CASE WHEN EXISTS (
+            SELECT 1
+            FROM public.products_characters T1
+            WHERE T1.character_id = T0.id
+            AND T1.product_id = :productId
+        ) THEN true ELSE false END) AS assigned
     FROM public.characters T0
-        LEFT JOIN public.products_characters T1 ON T0.id = T1.character_id
-        LEFT JOIN public.products T2 ON T1.product_id = T2.id
-    WHERE T0.deleted_at IS NULL
 `;
